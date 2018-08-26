1d69935c23c2ed76cc86aa38b118947b09acc33a
taylus <therealbjohnson@gmail.com>
2018-08-25 20:02:08 -0500

    Handle command line args

--- GBDasm.ConsoleApp/Program.cs
+++ GBDasm.ConsoleApp/Program.cs
@@ -9,10 +9,8 @@ namespace GBDasm.ConsoleApp
     {
         public static void Main(string[] args)
         {
-            //TODO: make command-line args
-            const string fileName = "Super Mario Land.gb";
-            string inFile = $"roms\\{fileName}";
-            string outFile = Path.ChangeExtension(fileName, ".asm");
+            ParseCommandLineArgs(args, out string outFile, out string inFile);
+            if (inFile == null || outFile == null) return;
 
             var rom = new RomFile(inFile);
             var dasm = new Disassembler(rom, new Decoder());
@@ -22,6 +20,25 @@ namespace GBDasm.ConsoleApp
 
             Console.WriteLine($"Wrote \"{outFile}\"");
             Process.Start(new ProcessStartInfo() { FileName = outFile, UseShellExecute = true });
+        }
+
+        private static void ParseCommandLineArgs(string[] args, out string outFile, out string inFile)
+        {
+            if (args.Length == 1)
+            {
+                inFile = args[0];
+                outFile = Path.ChangeExtension(inFile, ".asm");
+            }
+            else if (args.Length == 3 && args[0] == "-o")
+            {
+                outFile = args[1];
+                inFile = args[2];
+            }
+            else
+            {
+                outFile = inFile = null;
+                Console.WriteLine("Usage: gbdasm [-o outfile] rom.gb");
+            }
         }
     }
 }
