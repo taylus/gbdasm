77f0ad4fd4e7ca137aeedfc423a6674395561c97
taylus <brandon@legacy.codes>
2020-02-09 16:44:40 -0600

    Fix test and warnings

--- GBDasm.Core.Test/Disassembler_Should.cs
+++ GBDasm.Core.Test/Disassembler_Should.cs
@@ -12,14 +12,14 @@ namespace GBDasm.Core.Test
         public void Disassemble_Test_Program()
         {
             var program = new byte[] { 0x00, 0x01, 0xAD, 0xDE, 0x00, 0x03 };
-            var dasm = new Disassembler(new RomFile(program) { HasHeader = false }, decoder);
+            var dasm = new Disassembler(new RomFile(program) { HasHeader = false }, decoder, targetLineWidth: 20);
 
             string expectedDisassembly =
                 $"SECTION \"rom0\", ROM0{Environment.NewLine}" +
-                $"nop ;0000{Environment.NewLine}" +
-                $"ld bc, $dead ;0001{Environment.NewLine}" +
-                $"nop ;0004{Environment.NewLine}" +
-                $"inc bc ;0005";
+                $"nop           ;$0000{Environment.NewLine}" +
+                $"ld bc, $dead  ;$0001{Environment.NewLine}" +
+                $"nop           ;$0004{Environment.NewLine}" +
+                $"inc bc        ;$0005";
 
             Assert.AreEqual(expectedDisassembly, dasm.Disassemble());
         }
